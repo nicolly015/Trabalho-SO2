da8f8560dce2a0e34485c41a31af6c5f18432f60
a <a>
2024-11-02 20:58:58 -0300

    plau

--- trabalho.sh
+++ trabalho.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
+#1 
+ sudo addgroup aluno
+ sudo addgroup professor
+ sudo addgroup gerente
 
-1-
-sudo groupadd aluno
-sudo groupadd professor
-sudo groupadd gerente
+#2
+ sudo mkdir /srv
+ sudo chown root:gerente /srv
 
-2-
-sudo mkdir -p /srv
-sudo chown root:gerente /srv
-sudo chmod 750 /srv
+#3
+ sudo mkdir /srv/alunos
+ sudo mkdir /srv/professores
+ sudo chown root:aluno /srv/alunos
+ sudo chown root:professor /srv/professores
 
-3-
-sudo mkdir /srv/alunos
-sudo mkdir /srv/professores
-sudo chown root:aluno /srv/alunos
-sudo chown root:professor /srv/professores
+#4
+ sudo chmod 751 /srv
+ sudo chmod 770 /srv/alunos 
+ sudo chmod 770 /srv/professores
 
-4-
-sudo chmod 750 /srv
-sudo chmod 770 /srv/alunos
-sudo chmod 770 /srv/professores
+#5
+ 
+ sudo adduser --ingroup professor --home /srv/professores/bruno --gecos "" --disabled-login bruno
 
-5-
-sudo useradd -m -d /srv/professores/bruno -g professor --gecos "" --disabled-login bruno
+#6
+ sudo usermod -aG gerente bruno
 
-6-
-sudo usermod -aG gerente bruno
+#7
+ sudo adduser --ingroup aluno --home /srv/alunos/maria --gecos "" --disabled-login maria
+ sudo adduser --ingroup aluno --home /srv/alunos/joao --gecos "" --disabled-login joao
 
-7-
-sudo useradd -m -d /srv/alunos/joao -g aluno --gecos "" --disabled-login joao
-sudo useradd -m -d /srv/alunos/maria -g aluno --gecos "" --disabled-login maria
-
-8-
-sudo chage -E 2024-12-31 joao
-sudo chage -E 2024-12-31 maria
-sudo chage -M 90 joao
-sudo chage -M 90 maria
-
-
+#8
+ sudo usermod -e 2024-12-31 -f 90 joao
+ sudo usermod -e 2024-12-31 -f 90 maria
